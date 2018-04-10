e688fb2597411ddde9fe812294df27811e0b1317
Matthias Kornexl <mlkornexl@netscape.net>
2018-04-10 21:49:35 +0200

    minor bug fix getConfig

--- R/getConfig.R
+++ R/getConfig.R
@@ -1,6 +1,8 @@
 #' Get Configuration Data from Redis Cache
 #'
 #' @param key character string, key to identify config data in Redis cache
+#' @param auto_load character string, file path of config file to be loaded
+#'     automatically, if cached config is \code{NULL}.
 #'
 #' @return an object deparsed from JSON string that is stored in Redis cache
 #'     with key \code{key}.
@@ -15,10 +17,16 @@
 #'
 #' @export
 #'
-redis_getConfig <- function(key) {
+redis_getConfig <- function(key, auto_load) {
   if (redis_connect()) on.exit(rredis::redisClose())
 
-  if (!rredis::redisExists(key)) return(NULL)
+  if (!rredis::redisExists(key)) {
+    if (missing(auto_load)) {
+      return(NULL)
+    } else {
+      return(redis_loadConfig(auto_load))
+    }
+  }
 
   config <- rredis::redisGet(key) %>%
     jsonlite::fromJSON()
